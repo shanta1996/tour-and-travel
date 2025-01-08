b3cf8b0cd937d97a9adbe80047d9c676892c3480
shanta1996 <shantachaudhary490@gmail.com>
2025-01-08 15:25:32 +0545

    changes

--- backend/tourntravel/destinationapi/models.py
+++ backend/tourntravel/destinationapi/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Country(models.Model):
+    country_name=models.CharField(max_length=100)
+    capital_name=models.CharField(max_length=100)
+    languages=models.CharField(max_length=300)
+    regigions=models.CharField(max_length=300)
+    telephone_code=models.CharField(max_length=150)
+    image=models.CharField(max_length=100)
