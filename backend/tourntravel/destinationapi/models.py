b390390e50b37b105d798a146443ef2aa05d1389
shanta1996 <shantachaudhary490@gmail.com>
2025-01-08 16:00:25 +0545

    changes

--- backend/tourntravel/destinationapi/models.py
+++ backend/tourntravel/destinationapi/models.py
@@ -5,6 +5,9 @@ class Country(models.Model):
     country_name=models.CharField(max_length=100)
     capital_name=models.CharField(max_length=100)
     languages=models.CharField(max_length=300)
-    regigions=models.CharField(max_length=300)
+    religions=models.CharField(max_length=300)
     telephone_code=models.CharField(max_length=150)
-    image=models.CharField(max_length=100)
+    image=models.FileField(upload_to='static/uploads')
+
+    def __str__(self):
+        return self.country_name
